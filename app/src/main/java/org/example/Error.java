2a77adfa0b1118c735a04fc9f01b60a641541134
hung120303 <vuh26@up.edu>
2024-11-24 14:42:10 -0800

    alphabet parser

--- app/src/main/java/org/example/Error.java
+++ app/src/main/java/org/example/Error.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import javax.swing.*;
-import java.util.Set;
-import java.util.HashSet;
+import javax.swing.JOptionPane;
 
 
 public class Error {
@@ -42,6 +40,49 @@ public class Error {
 		}	
 		return val;
 	}
+
+	public String promptSigma(){
+		boolean valid = false;
+		String s = "";
+
+		while(!valid) {
+			try {
+				//Store input into String
+				s = JOptionPane.showInputDialog("Enter your alphabet (Use numbers (0-9), uppercase/lowercase letters (A-Z, a-z))(Ex: a,b,1,0): ");
+
+				//If s is null, user pressed cancel or closed the window. exit the program
+				if(s == null){
+					JOptionPane.showMessageDialog(null, "Exiting program.");
+					System.exit(0);
+				}
+
+
+				int i = 0;
+				boolean pass = true;
+				while(i < s.length()){ //enumerate through each char element of the intput string
+					char cur = s.charAt(i);
+					if( ((int)cur > 47 && (int)cur < 58) || ((int)cur > 64 && (int)cur < 91) || ((int)cur > 96 && (int)cur < 123) || (int)cur == 44 || (int)cur == 3 || (int)cur == 32){ // If ascii value is 0-9, A-Za-z, ,
+						i++;
+					}
+					else{
+						pass = false;
+					}
+
+				}
+				if(!pass){
+					JOptionPane.showMessageDialog(null, "Invalid input (Use numbers (0-9), uppercase/lowercase letters (A-Z, a-z))(Ex: a,b,1,0). Try again.");
+				}
+				else{
+					valid = true;
+				}
+			}
+			catch(Exception e) {
+				JOptionPane.showMessageDialog(null, "Invalid input (Use numbers (0-9), uppercase/lowercase letters (A-Z, a-z))(Ex: a,b,1,0). Try again.");
+			}
+		}
+		return s;
+	}
+
 	
 	
 }
